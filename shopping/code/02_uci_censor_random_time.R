ee0d2bc85455f1c7f1ea5c1adce547e93bc1da1c
Aaron Tang <aarontang@169-231-87-55.wireless.ucsb.edu>
2026-03-03 15:47:40 -0800

    finalized documentation

--- shopping/code/02_uci_censor_random_time.R
+++ shopping/code/02_uci_censor_random_time.R
@@ -9,7 +9,7 @@ uci_data <- read_csv(file.path(work_dir, "shopping", "data", "intermediate_data"
 set.seed(123)
 time_threshold <- runif(nrow(uci_data), min = 10, max = 1800) # random time threshold is between 30 and 60 min
 
-uci_transformed <- uci_data_summed %>%
+uci_transformed <- uci_data %>%
   mutate(TimeThreshold = time_threshold, # different time threshold for every row 
          CensorTime = ifelse(TotalDuration >= time_threshold, time_threshold, TotalDuration), # time with censoring
          Censor = ifelse(TotalDuration >= time_threshold, 1, 0)) # censor indicator
@@ -19,6 +19,9 @@ uci_transformed %>%
   group_by(Censor) %>%
   summarize(n = n())
 
-write.csv(uci_transformed, file.path(work_dir, "shopping", "data", "intermediate_data", "03_uci_random_censor.csv"),
+write.csv(uci_transformed, file.path(work_dir, "shopping", "data", "intermediate_data", "02_uci_random_censor.csv"),
+          row.names = FALSE)
+
+write.csv(uci_transformed, file.path(work_dir, "shopping", "data", "final_data", "shopping_data_final.csv"),
           row.names = FALSE)
 
